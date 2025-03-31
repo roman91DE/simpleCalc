9931fe5e811c36a98aad9a891c5a0da012002308
rohoehn <rohoehn123@gmail.com>
2025-03-31 15:56:51 +0200

    Evaluator seems to work

--- app/Evaluator.hs
+++ app/Evaluator.hs
@@ -1,17 +1,15 @@
-
 module Evaluator where
 
-import Data.Char (isDigit, isAlpha, isAlphaNum, isSpace)
-import Text.Read (readMaybe)
+import Data.Char (isAlpha, isAlphaNum, isDigit, isSpace)
 
 -- Expression AST
 data Expr
-    = Const Double         -- Literal number
-    | Add Expr Expr        -- (+ a b)
-    | Sub Expr Expr        -- (- a b)
-    | Mul Expr Expr        -- (* a b)
-    | Div Expr Expr        -- (/ a b)
-    deriving (Show, Eq)
+  = Const Double -- Literal number
+  | Add Expr Expr -- (+ a b)
+  | Sub Expr Expr -- (- a b)
+  | Mul Expr Expr -- (* a b)
+  | Div Expr Expr -- (/ a b)
+  deriving (Show, Eq)
 
 operatorSymbols :: [Char]
 operatorSymbols = "+-*/'"
@@ -23,13 +21,13 @@ tokenize :: String -> [String]
 tokenize = go []
   where
     go acc [] = reverse acc
-    go acc (c:cs)
+    go acc (c : cs)
       | isSpace c = go acc cs
       | c `elem` (operatorSymbols ++ bracesSymbols) =
           go ([c] : acc) cs
       | isDigit c || isAlpha c || c == '.' =
-          let (tok, rest) = span isIdentChar (c:cs)
-          in go (tok : acc) rest
+          let (tok, rest) = span isIdentChar (c : cs)
+           in go (tok : acc) rest
       | otherwise = go acc cs
 
     isIdentChar x = isAlphaNum x || x == '.'
@@ -40,25 +38,45 @@ lookupOp "+" = Just Add
 lookupOp "-" = Just Sub
 lookupOp "*" = Just Mul
 lookupOp "/" = Just Div
-lookupOp _   = Nothing
+lookupOp _ = Nothing
 
 -- Recursive parser for fully parenthesized prefix expressions
 parseExpr :: [String] -> Maybe (Expr, [String])
 parseExpr [] = Nothing
-parseExpr ("(":tok:rest)
-    | Just op <- lookupOp tok = do
-        (lhs, rest1) <- parseExpr rest
-        (rhs, rest2) <- parseExpr rest1
-        case rest2 of
-            (")":rest3) -> return (op lhs rhs, rest3)
-            _           -> Nothing  -- missing closing paren
-parseExpr (tok:rest)
-    | Just n <- readMaybe tok = Just (Const n, rest)
+parseExpr ("(" : tok : rest)
+  | Just op <- lookupOp tok = do
+      (lhs, rest1) <- parseExpr rest
+      (rhs, rest2) <- parseExpr rest1
+      case rest2 of
+        (")" : rest3) -> return (op lhs rhs, rest3)
+        _ -> Nothing -- missing closing paren
+parseExpr (tok : rest)
+  | Just n <- readMaybe tok = Just (Const n, rest)
 parseExpr _ = Nothing
 
 -- Parse expression from raw string
 readExpr :: String -> Maybe Expr
 readExpr s = case parseExpr (tokenize s) of
-    Just (expr, []) -> Just expr
-    _               -> Nothing
+  Just (expr, []) -> Just expr
+  _ -> Nothing
 
+evalNode :: Expr -> Maybe Double
+evalNode (Const d) = Just d
+evalNode (Add l r) = do
+  lv <- evalNode l
+  rv <- evalNode r
+  return (lv + rv)
+evalNode (Sub l r) = do
+  lv <- evalNode l
+  rv <- evalNode r
+  return (lv - rv)
+evalNode (Mul l r) = do
+  lv <- evalNode l
+  rv <- evalNode r
+  return (lv * rv)
+evalNode (Div l r) = do
+  lv <- evalNode l
+  rv <- evalNode r
+  if rv /= 0
+    then return (lv / rv)
+    else Nothing
--- app/Main.hs
+++ app/Main.hs
@@ -4,14 +4,14 @@ import Evaluator
 
 main :: IO ()
 main = loop
-    where
-        loop :: IO ()
-        loop = do
-            putStr "Enter Expression (:q to exit): "
-            input <- getLine
-            if input == ":q"
-               then putStrLn "Goodbye!"
-               else do
-                   let result = evalExpr $ parseExpr $ tokenize $ input
-                   putStrLn $ "Result: " ++ result
-                   loop
+  where
+    loop :: IO ()
+    loop = do
+      putStr "Enter Expression (:q to exit): "
+      input <- getLine
+      if input == ":q"
+        then putStrLn "Goodbye!"
+        else do
+          let result = evalExpr $ parseExpr $ tokenize $ input
+          putStrLn $ "Result: " ++ result
+          loop
