09e6a71bb116512ba34677fc82811091697756e7
rohoehn <rohoehn123@gmail.com>
2025-03-31 11:42:49 +0200

    parser works

---
+++ app/Evaluator.hs
@@ -0,0 +1,64 @@
+
+module Evaluator where
+
+import Data.Char (isDigit, isAlpha, isAlphaNum, isSpace)
+import Text.Read (readMaybe)
+
+-- Expression AST
+data Expr
+    = Const Double         -- Literal number
+    | Add Expr Expr        -- (+ a b)
+    | Sub Expr Expr        -- (- a b)
+    | Mul Expr Expr        -- (* a b)
+    | Div Expr Expr        -- (/ a b)
+    deriving (Show, Eq)
+
+operatorSymbols :: [Char]
+operatorSymbols = "+-*/'"
+
+bracesSymbols :: [Char]
+bracesSymbols = "()"
+
+tokenize :: String -> [String]
+tokenize = go []
+  where
+    go acc [] = reverse acc
+    go acc (c:cs)
+      | isSpace c = go acc cs
+      | c `elem` (operatorSymbols ++ bracesSymbols) =
+          go ([c] : acc) cs
+      | isDigit c || isAlpha c || c == '.' =
+          let (tok, rest) = span isIdentChar (c:cs)
+          in go (tok : acc) rest
+      | otherwise = go acc cs
+
+    isIdentChar x = isAlphaNum x || x == '.'
+
+-- Operator lookup table
+lookupOp :: String -> Maybe (Expr -> Expr -> Expr)
+lookupOp "+" = Just Add
+lookupOp "-" = Just Sub
+lookupOp "*" = Just Mul
+lookupOp "/" = Just Div
+lookupOp _   = Nothing
+
+-- Recursive parser for fully parenthesized prefix expressions
+parseExpr :: [String] -> Maybe (Expr, [String])
+parseExpr [] = Nothing
+parseExpr ("(":tok:rest)
+    | Just op <- lookupOp tok = do
+        (lhs, rest1) <- parseExpr rest
+        (rhs, rest2) <- parseExpr rest1
+        case rest2 of
+            (")":rest3) -> return (op lhs rhs, rest3)
+            _           -> Nothing  -- missing closing paren
+parseExpr (tok:rest)
+    | Just n <- readMaybe tok = Just (Const n, rest)
+parseExpr _ = Nothing
+
+-- Parse expression from raw string
+readExpr :: String -> Maybe Expr
+readExpr s = case parseExpr (tokenize s) of
+    Just (expr, []) -> Just expr
+    _               -> Nothing
+
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import Evaluator
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = loop
+    where
+        loop :: IO ()
+        loop = do
+            putStr "Enter Expression (:q to exit): "
+            input <- getLine
+            if input == ":q"
+               then putStrLn "Goodbye!"
+               else do
+                   let result = evalExpr $ parseExpr $ tokenize $ input
+                   putStrLn $ "Result: " ++ result
+                   loop
